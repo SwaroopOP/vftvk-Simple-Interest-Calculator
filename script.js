cdfd10ac200ddaae6e0da60381270faba8b15a99
SwaroopOP <swaroopgochbsccs2019@student.mes.ac.in>
2021-02-23 12:49:34 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
-{
+{   
+    let principal
+    principal="principal";
     p = document.getElementById("principal").value;
-    
-}
-        
+    if (principal=0)
+        <script>window.alert("Enter a positive number");</script>
+    if (principal<0)
+        <script>window.alert("Enter a positive number");</script>
+}      
+
+    let rate
+    rate="rate";
+    let years
+    years="years";
+    let interest
+    interest="principal * years * rate / 100";
+    <div><select id="mySelect" onchange="myFunction()"></select>
+    <span id="slider_value" style="color:black;"></span></div>
+<div>
+<button onclick="myFunction()">Compute Interest</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "If you deposit <mark>1000000</mark>,<br>at an interest rate of <mark>3.5%</mark>.<br>You will receive an amount of <mark>175000</mark>,<br>in the year <mark>2025</mark>"}
+</script>
+</div>
