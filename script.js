145ed719505b3f80616dcb12059faece075da4a8
SwaroopOP <swaroopgochbsccs2019@student.mes.ac.in>
2021-02-24 12:08:03 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 function compute()
 {   
-    let principal
+    
     principal="principal";
     p = document.getElementById("principal").value;
-    if (principal=0)
-        <script>window.alert("Enter a positive number");</script>
-    if (principal<0)
-        <script>window.alert("Enter a positive number");</script>
-}      
+if(p<=0)
 
-    let rate
-    rate="rate";
-    let years
-    years="years";
-    let interest
-    interest="principal * years * rate / 100";
-    <div><select id="mySelect" onchange="myFunction()"></select>
-    <span id="slider_value" style="color:black;"></span></div>
-<div>
-<button onclick="myFunction()">Compute Interest</button>
+{
 
-<p id="demo"></p>
+   alert("enter a positive number greater than zero");
 
-<script>
-function myFunction() {
-  document.getElementById("demo").innerHTML = "If you deposit <mark>1000000</mark>,<br>at an interest rate of <mark>3.5%</mark>.<br>You will receive an amount of <mark>175000</mark>,<br>in the year <mark>2025</mark>"}
-</script>
-</div>
+   document.getElementById("prinicipal").focus();
+
+   document.getElementById("prinicipal").click();
+   <script>
+    function Compute Interest() {
+    document.getElementById("demo").innerHTML = "If you deposit <mark>1000000</mark>,<br>at an interest rate of <mark>3.5%</mark>.<br>You will receive an amount of <mark>175000</mark>,<br>in the year <mark>2025</mark>"}</script>
+   return null;
+
+}   
+}  
