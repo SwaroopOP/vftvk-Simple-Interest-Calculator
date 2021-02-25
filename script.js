e6f059101ace0666d5ddc2bf73bfadafcc22173e
SwaroopOP <swaroopgochbsccs2019@student.mes.ac.in>
2021-02-25 11:55:37 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 function compute()
 {   
-    
-    principal="principal";
-    p = document.getElementById("principal").value;
-if(p<=0)
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    interest= principal * years * rate / 100;
+    years_in_future= newDate().getFullYear() + parseInt(years);
+if(principal<=0)
 
 {
 
    alert("enter a positive number greater than zero");
 
    document.getElementById("prinicipal").focus();
-
    document.getElementById("prinicipal").click();
-   <script>
-    function Compute Interest() {
-    document.getElementById("demo").innerHTML = "If you deposit <mark>1000000</mark>,<br>at an interest rate of <mark>3.5%</mark>.<br>You will receive an amount of <mark>175000</mark>,<br>in the year <mark>2025</mark>"}</script>
+}
+else{
+    result.innerHTML = "If you deposit"+"<mark>"+"principal"+"</mark>"+"," +"</br>"+"at an interest rate of"+"<mark>"+"rate"+"%"+"</mark>"+"."+"</br>"+"You will receive an amount of"+"<mark>"+"years"+"</mark>"+","+"</br>"+"in the year"+"<mark>"+"years_in_future"+"</mark>"
+}
    return null;
-
-}   
-}  
+}
+function updateSlider(sliderAmount)
+{
+    var sliderSpan= document.getElementById("sliderAmount")
+    sliderSpan.innerHTML =sliderAmount +"%";
+}
