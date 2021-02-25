ca5049ae9bfdcb35e18a00d3906a13a145ca329e
SwaroopOP <swaroopgochbsccs2019@student.mes.ac.in>
2021-02-25 12:17:24 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 function compute()
 {   
     principal = document.getElementById("principal").value;
-    rate = document.getElementById("rate").value;
-    years = document.getElementById("years").value;
-    interest= principal * years * rate / 100;
-    years_in_future= newDate().getFullYear() + parseInt(years);
-if(principal<=0)
+            rate = document.getElementById("rate").value;
+            years = document.getElementById("years").value;
+    interest = principal * years *(rate/100);
+            years_in_future= newDate().getFullYear() + parseInt(years);
 
-{
-
-   alert("enter a positive number greater than zero");
-
-   document.getElementById("prinicipal").focus();
-   document.getElementById("prinicipal").click();
+//validation for Principal input box
+            if(principal<=0){
+                window.alert("enter a positive number greater than zero");
+                document.getElementById("prinicipal").focus();
+            }
+            else{
+                result.innerHTML = "If you deposit"+"<mark>"+"principal"+"</mark>"+"," +"</br>"+"at an interest rate of"+"<mark>"+"rate"+"%"+"</mark>"+"."+"</br>"+"You will receive an amount of"+"<mark>"+"years"+"</mark>"+","+"</br>"+"in the year"+"<mark>"+"years_in_future"+"</mark>"
+            }  
 }
-else{
-    result.innerHTML = "If you deposit"+"<mark>"+"principal"+"</mark>"+"," +"</br>"+"at an interest rate of"+"<mark>"+"rate"+"%"+"</mark>"+"."+"</br>"+"You will receive an amount of"+"<mark>"+"years"+"</mark>"+","+"</br>"+"in the year"+"<mark>"+"years_in_future"+"</mark>"
-}
-   return null;
-}
+//updating slider
 function updateSlider(sliderAmount)
 {
     var sliderSpan= document.getElementById("sliderAmount")
